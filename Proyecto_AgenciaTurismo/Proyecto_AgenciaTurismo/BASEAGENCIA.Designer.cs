c3037dccb0368e21501e699ce82522f27f704f15
Noralma92 <70734179+Noralma92@users.noreply.github.com>
2020-09-13 12:06:35 -0500

    Add files via upload

---
+++ Proyecto_AgenciaTurismo/Proyecto_AgenciaTurismo/BASEAGENCIA.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Proyecto_AgenciaTurismo
+{
+    partial class BaseAgencia
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BaseAgencia));
+            this.lblAgencia = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLitoral = new System.Windows.Forms.Button();
+            this.btnCosta = new System.Windows.Forms.Button();
+            this.btnSierra = new System.Windows.Forms.Button();
+            this.btnAmazonia = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAgencia
+            // 
+            this.lblAgencia.AutoSize = true;
+            this.lblAgencia.BackColor = System.Drawing.Color.Black;
+            this.lblAgencia.Font = new System.Drawing.Font("Harrington", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAgencia.ForeColor = System.Drawing.Color.Transparent;
+            this.lblAgencia.Location = new System.Drawing.Point(122, 22);
+            this.lblAgencia.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAgencia.Name = "lblAgencia";
+            this.lblAgencia.Size = new System.Drawing.Size(688, 32);
+            this.lblAgencia.TabIndex = 0;
+            this.lblAgencia.Text = "AGENCIA TURISTICA \" ECUADOR SIN FRONTERAS\".";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(352, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DESTINOS TURISTICOS";
+            // 
+            // btnLitoral
+            // 
+            this.btnLitoral.BackColor = System.Drawing.Color.Black;
+            this.btnLitoral.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnLitoral.Location = new System.Drawing.Point(217, 400);
+            this.btnLitoral.Name = "btnLitoral";
+            this.btnLitoral.Size = new System.Drawing.Size(117, 54);
+            this.btnLitoral.TabIndex = 2;
+            this.btnLitoral.Text = "REGIÓN LITORAL ";
+            this.btnLitoral.UseVisualStyleBackColor = false;
+            this.btnLitoral.Click += new System.EventHandler(this.btnLitoral_Click);
+            // 
+            // btnCosta
+            // 
+            this.btnCosta.BackColor = System.Drawing.Color.Black;
+            this.btnCosta.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCosta.Location = new System.Drawing.Point(635, 400);
+            this.btnCosta.Name = "btnCosta";
+            this.btnCosta.Size = new System.Drawing.Size(126, 54);
+            this.btnCosta.TabIndex = 3;
+            this.btnCosta.Text = "REGIÓN COSTA";
+            this.btnCosta.UseVisualStyleBackColor = false;
+            this.btnCosta.Click += new System.EventHandler(this.btnCosta_Click);
+            // 
+            // btnSierra
+            // 
+            this.btnSierra.BackColor = System.Drawing.Color.Black;
+            this.btnSierra.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSierra.Location = new System.Drawing.Point(217, 670);
+            this.btnSierra.Name = "btnSierra";
+            this.btnSierra.Size = new System.Drawing.Size(117, 56);
+            this.btnSierra.TabIndex = 4;
+            this.btnSierra.Text = "REGIÓN SIERRA";
+            this.btnSierra.UseVisualStyleBackColor = false;
+            this.btnSierra.Click += new System.EventHandler(this.btnSierra_Click);
+            // 
+            // btnAmazonia
+            // 
+            this.btnAmazonia.BackColor = System.Drawing.Color.Black;
+            this.btnAmazonia.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnAmazonia.Location = new System.Drawing.Point(635, 670);
+            this.btnAmazonia.Name = "btnAmazonia";
+            this.btnAmazonia.Size = new System.Drawing.Size(126, 56);
+            this.btnAmazonia.TabIndex = 5;
+            this.btnAmazonia.Text = "REGION AMAZÓNICA";
+            this.btnAmazonia.UseVisualStyleBackColor = false;
+            this.btnAmazonia.Click += new System.EventHandler(this.btnAmazonia_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(352, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "ECUADOR Y SUS PAISAJES";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(146, 213);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(274, 181);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(558, 224);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(252, 170);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(146, 486);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(249, 167);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(543, 497);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(298, 156);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(-8, -1);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(975, 759);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(227, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(523, 22);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Estimado Usuraio, eliga uno de las siguientes regiones que desee visitar.\r\n";
+            // 
+            // BaseAgencia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(968, 749);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAmazonia);
+            this.Controls.Add(this.btnSierra);
+            this.Controls.Add(this.btnCosta);
+            this.Controls.Add(this.btnLitoral);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblAgencia);
+            this.Controls.Add(this.pictureBox5);
+            this.Font = new System.Drawing.Font("Bell MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "BaseAgencia";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAgencia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLitoral;
+        private System.Windows.Forms.Button btnCosta;
+        private System.Windows.Forms.Button btnSierra;
+        private System.Windows.Forms.Button btnAmazonia;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Proyecto_AgenciaTurismo/Proyecto_AgenciaTurismo/BASEAGENCIA.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_AgenciaTurismo
+{
+    public partial class BaseAgencia : Form
+    {
+        public BaseAgencia()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLitoral_Click(object sender, EventArgs e)
+        {
+            REGIONLITORAL regionlitoral = new REGIONLITORAL();
+            regionlitoral.Show();
+
+            BaseAgencia baseAgencia = new BaseAgencia();
+            baseAgencia.Close();
+
+        }
+
+        private void btnCosta_Click(object sender, EventArgs e)
+        {
+            REGIONCOSTA regioncosta = new REGIONCOSTA();
+
+            regioncosta.Show();
+            BaseAgencia baseAgencia = new BaseAgencia();
+            baseAgencia.Close();
+
+
+        }
+
+        private void btnSierra_Click(object sender, EventArgs e)
+        {
+            REGIONSIERRA regionsierra = new REGIONSIERRA();
+
+            regionsierra.Show();
+
+            BaseAgencia baseAgencia = new BaseAgencia();
+            baseAgencia.Close();
+
+        }
+
+        private void btnAmazonia_Click(object sender, EventArgs e)
+        {
+            REGIONAMAZONIA regionamazonica = new REGIONAMAZONIA();
+
+            regionamazonica.Show();
+
+            BaseAgencia baseAgencia = new BaseAgencia();
+            baseAgencia.Close();
+
+        }
+    }
+}
